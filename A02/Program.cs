7b1ec1512334a7b92b698b4cf2401864641bd05e
yoganathan <yoganathan.m@metamation.com>
2023-07-03 12:05:53 +0530

    Changes Applied

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 #region Exercise A02 - Guessing Game
-Random rnd = new Random ();
-int number = rnd.Next (1, 101);
+int number = new Random ().Next (1, 101);
+Console.WriteLine ("To exit type exit and press enter");
 Console.WriteLine ("Guess the number between 1 to 100");
 for (; ; ) {
    Console.Write ("Enter guessed number :");
    string guessNumber = Console.ReadLine ();
-   int output;
-   bool check = int.TryParse (guessNumber, out output);
-   if (check == true) {
+   bool check = int.TryParse (guessNumber, out int output);
+   if (check) {
       if (output == number) {
          Console.WriteLine ($"Guessed number is {number} ");
          break;
       } else if (output > number) Console.WriteLine ("Number is high");
       else Console.WriteLine ("Number is low");
-   } else { Console.WriteLine ("Given Input is not an integer"); }
+   } else if (guessNumber.ToString ().ToLower () == "exit")
+      break;
+   else Console.WriteLine ("Given input is not an valid number");
 }
 #endregion
