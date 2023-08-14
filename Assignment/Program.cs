6c25150a3c4096428862c800c74589208cd27004
yoganathan <yoganathan.m@metamation.com>
2023-08-14 15:34:02 +0530

    T05 Completed

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
+﻿for (; ; ) {
+   Console.Write ("Enter number to be checked :");
+   if (int.TryParse (Console.ReadLine (), out int num)) {
+      int count = 0;
+      for (int i = 2; i < num; i++) {
+         if (num % i != 0)
+            count++;
+         else {
+            Console.Write ($"{num} is not a prime number");
+            break;
+         }
+         if (count == num - 2)
+            Console.Write ($"{num} is a prime number");
       }
-   }
+      break;
+   } else Console.WriteLine ("Enter valid number");
 }
