03e42fb3fd3fef584996a996ae34047933799e8a
yoganathan <yoganathan.m@metamation.com>
2023-08-14 16:35:25 +0530

    T06 Refractor change

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,13 +1,20 @@
-﻿Console.Write ("Enter word or sentence : ");
-string word = Console.ReadLine ().ToLower ().Replace (" ", "");
-int wordCount = word.Length, count = 0;
-for (int i = 0; i < word.Length; i++) {
-   if (word[i] == word[word.Length - 1 - i])
-      count++;
-   else
-      break;
+﻿for (; ; ) {
+   Console.Write ("Enter word or sentence : ");
+   string word = Console.ReadLine ().ToLower ().Replace (" ", "");
+   int wordCount = word.Length, count = 0;
+   if (wordCount > 0) {
+      for (int i = 0; i < word.Length; i++) {
+         if (word[i] == word[word.Length - 1 - i])
+            count++;
+         else
+            break;
+      }
+      if (count == wordCount) {
+         Console.WriteLine ("palindrome");
+         break;
+      } else {
+         Console.WriteLine ("Not a palindrome");
+         break;
+      }
+   } else Console.WriteLine ("Empty please enter a text to check");
 }
-if (count == wordCount)
-   Console.WriteLine ("palindrome");
-else
-   Console.WriteLine ("Not a palindrome");
