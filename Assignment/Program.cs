f0191678fe1ea3c1fb654c07ba09d10d7d0890f9
yoganathan <yoganathan.m@metamation.com>
2023-08-14 15:28:18 +0530

    T03 Completed

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,12 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
+﻿for (; ; ) {
+   Console.Write ("Enter input number :");
+   if (ulong.TryParse (Console.ReadLine (), out ulong n)) {
+      ulong firstTerm = 0, secondTerm = 1;
+      for (ulong i = 0; i < n; i++) {
+         Console.WriteLine ($"f{i} : {firstTerm}");
+         ulong thirdTerm = firstTerm + secondTerm;
+         (firstTerm, secondTerm) = (secondTerm, thirdTerm);
       }
-   }
+      break;
+   } else Console.WriteLine ("Enter a valid number");
 }
