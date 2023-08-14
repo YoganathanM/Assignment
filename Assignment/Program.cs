d35fa3e57c324f73451d7469b0a21ce956562af6
yoganathan <yoganathan.m@metamation.com>
2023-08-14 15:41:57 +0530

    T06 Completed

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
-      }
-   }
+﻿Console.Write ("Enter word or sentence : ");
+string word = Console.ReadLine ().ToLower ().Replace (" ", "");
+int wordCount = word.Length, count = 0;
+for (int i = 0; i < word.Length; i++) {
+   if (word[i] == word[word.Length - 1 - i])
+      count++;
+   else
+      break;
 }
+if (count == wordCount)
+   Console.WriteLine ("palindrome");
+else
+   Console.WriteLine ("Not a palindrome");
