5fff23c8605415f650db2e9f9051de38400e83cb
yoganathan <yoganathan.m@metamation.com>
2023-08-17 15:03:18 +0530

    Changes applied

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,20 +1,14 @@
 ﻿for (; ; ) {
    Console.Write ("Enter word or sentence : ");
    string word = Console.ReadLine ().ToLower ().Replace (" ", "");
-   int wordCount = word.Length, count = 0;
-   if (wordCount > 0) {
-      for (int i = 0; i < word.Length; i++) {
-         if (word[i] == word[word.Length - 1 - i])
-            count++;
-         else
-            break;
-      }
-      if (count == wordCount) {
-         Console.WriteLine ("palindrome");
-         break;
-      } else {
-         Console.WriteLine ("Not a palindrome");
-         break;
-      }
-   } else Console.WriteLine ("Empty please enter a text to check");
+   if (word.Length == 0) {
+      Console.WriteLine ("Empty please enter a text to check");
+      continue;
+   }
+   var reversed = string.Join ("", word.Reverse ());
+   if (word == reversed)
+      Console.WriteLine ($"{word} is a palindrome");
+   else
+      Console.WriteLine ($"{word} is not a palindrome");
+   break;
 }
