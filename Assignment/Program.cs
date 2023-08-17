0a9416fc78d1ed0b8bad0ef2711c7b3d7c77b042
yoganathan <yoganathan.m@metamation.com>
2023-08-17 12:45:52 +0530

    Changes applied

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,17 +1,13 @@
-﻿for (; ; ) {
-   Console.Write ("Enter number to be checked :");
-   if (int.TryParse (Console.ReadLine (), out int num)) {
-      int count = 0;
-      for (int i = 2; i < num; i++) {
-         if (num % i != 0)
-            count++;
-         else {
-            Console.Write ($"{num} is not a prime number");
-            break;
-         }
-         if (count == num - 2)
-            Console.Write ($"{num} is a prime number");
-      }
+﻿Console.Write ("Enter number to be checked :");
+int num;
+while (!int.TryParse (Console.ReadLine (), out num))
+   Console.WriteLine ("Enter valid number");
+bool isPrime = true;
+for (int i = 2; i < num; i++) {
+   if (num % i == 0) {
+      isPrime = false;
+      Console.Write ($"{num} is not a prime number");
       break;
-   } else Console.WriteLine ("Enter valid number");
+   }
 }
+if (isPrime) Console.Write ($"{num} is a prime number");
