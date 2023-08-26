1777803d13679241e412de1ee20a7c140f96d529
yoganathan <yoganathan.m@metamation.com>
2023-08-26 19:55:24 +0530

    T08 Completed

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,9 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
-      }
-   }
+﻿while (true) {
+   Console.Write ("Enter multiplication number :");
+   if (int.TryParse (Console.ReadLine (), out int n) && n > 0) {
+      for (int i = 1; i <= 12; i++)
+         Console.WriteLine ($"{n} x {i,2} = {n * i}");
+      break;
+   } else
+      Console.WriteLine ("Enter valid number");
 }
