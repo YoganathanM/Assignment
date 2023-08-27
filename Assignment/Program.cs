7ee6acb93a582ef92d80c090334af1165ecdba29
yoganathan <yoganathan.m@metamation.com>
2023-08-27 09:36:30 +0530

    T10 completed

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,19 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
+﻿int reverse = 0;
+while (true) {
+   Console.Write ("Enter number :");
+   if (int.TryParse (Console.ReadLine (), out int originalNumber) && originalNumber > 0) {
+      int number = originalNumber;
+      while (number > 0) {
+         int remainder = number % 10;
+         reverse = reverse * 10 + remainder;
+         number = number / 10;
       }
-   }
+      Console.WriteLine ($"Reverse number is {reverse}");
+      if (originalNumber == reverse)
+         Console.WriteLine ($"{originalNumber} is a palindrome");
+      else
+         Console.WriteLine ($"{originalNumber} is not a palindrome");
+      break;
+   } else
+      Console.WriteLine ("Give valid number");
 }
