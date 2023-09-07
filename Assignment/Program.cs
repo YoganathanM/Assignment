20396ec5e75de3b4557cf6014b4d5faae3c473c7
yoganathan <yoganathan.m@metamation.com>
2023-09-07 22:41:38 +0530

    T11 Completed

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
+﻿while (true) {
+   Console.Write ("Enter number : ");
+   if (int.TryParse (Console.ReadLine (), out int num) && num > 0) {
+      int sum = 0;
+      do {
+         while (num > 0) {
+            sum = sum + num % 10;
+            num /= 10;
+         }
+         num = sum;
+         sum = 0;
       }
-   }
+      while (num >= 10);
+      Console.WriteLine ($"Digit root is {num}");
+      break;
+   } else
+      Console.WriteLine ("Give a valid number");
 }
