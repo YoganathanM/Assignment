1c1ea26cba66d6d38ebcd90fb6cef6fece27e9e4
yoganathan <yoganathan.m@metamation.com>
2023-09-08 18:43:21 +0530

    T15 Completed

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
-      }
+﻿Console.Write ("Enter text : ");
+string word = Console.ReadLine ();
+string str = word.ToLower ().Replace (" ", "");
+bool isogram = Isogram (str);
+if (isogram) Console.WriteLine ($"The given string {word} is an Isogram");
+else Console.WriteLine ($"The given string {word} is not an isogram");
+bool Isogram (string str) {
+   char[] s = str.ToCharArray ();
+   Array.Sort (s);
+   for (int i = 0; i < s.Length - 1; i++) {
+      if (s[i] == s[i + 1])
+         return false;
    }
+   return true;
 }
