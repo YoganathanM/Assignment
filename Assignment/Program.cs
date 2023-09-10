9a792545315628d8f644eb7475bdd5d54968a9fa
yoganathan <yoganathan.m@metamation.com>
2023-09-10 12:12:01 +0530

    T09 Completed.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,20 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
+﻿while (true) {
+   Console.Write ("Give number of rows : ");
+   if (int.TryParse (Console.ReadLine (), out int n) && (n > 0)) {
+      int i, l, s, j;
+      for (i = -n; i <= n; i++) {
+         //condition to change negative value of i to positive using l variable to calculate space
+         if (i < 0)
+            l = -i;
+         else
+            l = i;
+         for (s = 0; s < l; s++)
+            Console.Write ("  ");
+         for (j = 0; j < 2 * (n - l) + 1; j++)
+            Console.Write ("* ");
+         Console.WriteLine ();
       }
-   }
+      break;
+   } else
+      Console.WriteLine ("Give an valid number");
 }
