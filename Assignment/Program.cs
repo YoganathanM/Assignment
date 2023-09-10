e2aa86cd4006df4d77d7bfebc5f1948135345a0b
yoganathan <yoganathan.m@metamation.com>
2023-09-10 12:16:30 +0530

    T16 Completed.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
-      }
+﻿bool abecedarianWord = true;
+Console.Write ("Enter a string to validate : ");
+string word = Console.ReadLine ();
+string str = word.ToLower ().Replace (" ", "");
+char[] s = str.ToCharArray ();
+for (int i = 0; i < s.Length - 1; i++) {
+   if (s[i] > s[i + 1]) {
+      abecedarianWord = false;
+      break;
    }
 }
+if (abecedarianWord)
+   Console.WriteLine ($"Given word {word} is an abecedarian word");
+else
+   Console.WriteLine ($"Given word {word} is not an abecedarian word");
