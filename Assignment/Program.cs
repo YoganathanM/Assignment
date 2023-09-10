f7c679f7f7e4d876993e73677f3e81503ef20ef3
yoganathan <yoganathan.m@metamation.com>
2023-09-10 13:32:33 +0530

    T12 Completed.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,20 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
+﻿while (true) {
+   int i, s, j, l = 1;
+   Console.Write ("Give number of rows : ");
+   if (int.TryParse (Console.ReadLine (), out int n) && (n > 0)) {
+      for (i = 0; i < n; i++) {
+         for (s = 0; s < n - i - 1; s++)
+            Console.Write ("  ");
+         for (j = 0; j < i + 1; j++) {
+            if (j == 0 || i == 0)
+               l = 1;
+            else
+               l = l * (i - j + 1) / j;
+            Console.Write ($"   {l}");
+         }
+         Console.WriteLine ();
       }
-   }
+      break;
+   } else
+      Console.WriteLine ("Give an valid number");
 }
