6dac1c5d3037d1dd5f00bfd8cf09c60aefd19956
yoganathan <yoganathan.m@metamation.com>
2023-09-16 16:19:38 +0530

    T21-Implemented swap two numbers

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
-      }
-   }
+﻿Console.Write ("Enter a value : ");
+int a =int.Parse(Console.ReadLine ());
+Console.Write ("Enter b value : ");
+int b = int.Parse (Console.ReadLine ());
+swap (a, b);
+
+void swap (int a, int b) {
+   (a, b) = (b, a);
+   Console.Write ($"a value is : {a}\n");
+   Console.Write ($"b value is : {b}");
 }
