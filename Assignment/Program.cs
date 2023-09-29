875defdc2295ad051465641bc069d4a90a4b57f0
yoganathan <yoganathan.m@metamation.com>
2023-09-29 19:04:46 +0530

    T18.1-Implemented nth armstrong number.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,34 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
+﻿internal class Program {
+   private static void Main (string[] args) {
+      List<int> armstrongList = new List<int> ();
+      while (true) {
+         if (int.TryParse (args[0], out int n) && n >= 0 && n <= 25) {
+            var result = getArmstrongnumber (n);
+            Console.WriteLine ($"{n} index armstrong number is {result}");
+            break;
+         } else
+            Console.WriteLine ("Give valid number from 0 to 25");
+         break;
+      }
+      int getArmstrongnumber (int n) {
+         for (int i = 0; armstrongList.Count <= n; i++) {
+            double armstrong = 0, sum = 0;
+            int number, tempNumber, length;
+            string input = i.ToString ();
+            length = input.Length;
+            number = Convert.ToInt32 (input);
+            tempNumber = number;
+
+            while (tempNumber > 0) {
+               int rem = tempNumber % 10;
+               tempNumber = tempNumber / 10;
+               armstrong = Math.Pow (rem, length);
+               sum = sum + armstrong;
+            }
+            if (number == sum)
+               armstrongList.Add (number);
+         }
+         return armstrongList[n];
       }
    }
 }
