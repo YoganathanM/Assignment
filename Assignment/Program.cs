fc145a498bd8e38bc2bca665578357fed8c729dd
yoganathan <yoganathan.m@metamation.com>
2023-11-03 17:49:02 +0530

    Changes applied

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,18 +1,15 @@
-﻿while (true) {
-   Console.Write ("Enter number : ");
-   if (int.TryParse (Console.ReadLine (), out int num) && num > 0) {
-      int sum = 0;
-      do {
-         while (num > 0) {
-            sum = sum + num % 10;
-            num /= 10;
-         }
-         num = sum;
-         sum = 0;
+﻿//Check the digital root of a given number.
+Console.Write ("Enter number : ");
+if (int.TryParse (Console.ReadLine (), out int num) && num > 0) {
+   int sum = 0;
+   do {
+      while (num > 0) {
+         sum += num % 10;
+         num /= 10;
       }
-      while (num >= 10);
-      Console.WriteLine ($"Digit root is {num}");
-      break;
-   } else
-      Console.WriteLine ("Give a valid number");
-}
+      num = sum;
+      sum = 0;
+   } while (num >= 10);
+   Console.WriteLine ($"Digit root is {num}");
+} else
+   Console.WriteLine ("Give a valid number");
