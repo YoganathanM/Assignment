985eab9d13a37a54b6004d9af0abdb3df9efcc84
yoganathan <yoganathan.m@metamation.com>
2023-11-24 19:10:05 +0530

    Implemented factorial

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
-      }
+﻿// Find factorial for the given number.
+while (true) {
+   Console.Write ("Give number to find factorial : ");
+   if (int.TryParse (Console.ReadLine (), out int input) && (input > 0)) {
+      Factorial (input);
+      break;
+   } else Console.WriteLine ("Given input is an invalid number!");
+}
+void Factorial (int input) {
+   int fact = 1;
+   for (int i = 1; i <= input; i++) {
+      fact = fact * i;
    }
+   Console.WriteLine ($"Factorial of {input}! : {fact}");
 }
