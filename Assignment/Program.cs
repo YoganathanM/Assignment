054ffcd5903902f6ca39db7865b8daf007129fed
yoganathan <yoganathan.m@metamation.com>
2023-11-24 19:23:53 +0530

    Implemented swap indices

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,20 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
-      }
-   }
+﻿//Swap two Number from the given number list
+Random rnd = new Random ();
+int input = rnd.Next (5, 10);
+Console.Write ("Input numbers : ");
+List<int> numbers = new List<int> ();
+for (int i = 0; i < input; i++) {
+   int n = rnd.Next (1, 10);
+   numbers.Add (n);
+   Console.Write (n + " ");
 }
+Console.WriteLine ($"\nSwap two index values between 1 to {input}");
+Console.Write ("Give index to be swapped : ");
+int index1 = int.Parse (Console.ReadLine ()!);
+Console.Write ("Give index to be swapped : ");
+int index2 = int.Parse (Console.ReadLine ()!);
+SwapIndices (index1, index2);
+Console.Write ("Swapped output : ");
+for (int j = 0; j < numbers.Count; j++) Console.Write (numbers[j] + " ");
+
+void SwapIndices (int index1, int index2) => (numbers[index1 - 1], numbers[index2 - 1]) = (numbers[index2 - 1], numbers[index1 - 1]);
