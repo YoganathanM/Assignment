eafb85e4baa5e87bba323f1c0348a6479ce0d82b
yoganathan <yoganathan.m@metamation.com>
2023-12-28 18:57:41 +0530

    T23: Voting contest

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,21 @@
 ﻿namespace Assignment {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
+         Console.Write ("Enter total votes : ");
+         string input = Console.ReadLine ()!.ToLower ().Replace (" ", "");
+         VotingContext (input);
+      }
+
+      public static void VotingContext (string input) {
+         Dictionary<char, int> vote = new ();
+         for (int i = 0; i < input.Length; i++) {
+            if (vote.ContainsKey (input[i]))
+               vote[input[i]]++;
+            else
+               vote[input[i]] = 1;
+         }
+         var highestVote = vote.OrderByDescending (e => e.Value).First ();
+         Console.WriteLine ($"Winner is {highestVote.Key}");
       }
    }
 }
