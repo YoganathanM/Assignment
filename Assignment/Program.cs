5d6f4e7af471458954623328e2ae3f801b8b102c
yoganathan <yoganathan.m@metamation.com>
2023-12-29 18:32:54 +0530

    Exception handled

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,18 +1,21 @@
 ﻿namespace Assignment {
    internal class Program {
       static void Main (string[] args) {
-         Console.Write ("Enter total votes : ");
-         string input = Console.ReadLine ()!.ToLower ().Replace (" ", "");
-         VotingContext (input);
+         while(true) {
+            Console.Write ("Enter total votes : ");
+            string input = Console.ReadLine ()!.ToLower ().Replace (" ", "");
+            if (input.Length > 0) {
+               VotingContext (input);
+               break;
+            } else Console.WriteLine ("Input can't be empty");
+         }
       }
 
       public static void VotingContext (string input) {
          Dictionary<char, int> vote = new ();
          for (int i = 0; i < input.Length; i++) {
-            if (vote.ContainsKey (input[i]))
-               vote[input[i]]++;
-            else
-               vote[input[i]] = 1;
+            if (vote.ContainsKey (input[i])) vote[input[i]]++;
+            else vote[input[i]] = 1;
          }
          var highestVote = vote.OrderByDescending (e => e.Value).First ();
          Console.WriteLine ($"Winner is {highestVote.Key}");
