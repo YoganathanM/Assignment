88b53ce32f7733444f06ea6f114f20701f00173b
yoganathan <yoganathan.m@metamation.com>
2023-12-29 16:32:30 +0530

    Review 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,59 @@
-﻿namespace Assignment {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello world!");
+﻿namespace Training;
+class Spark {
+   public static void Main () {
+      int moneyGiven = 0;
+      int chocolatePrice = 0;
+      int wrapperValue = 0;
+      GetInput (ref moneyGiven, ref chocolatePrice, ref wrapperValue);
+      Console.WriteLine ();
+      GetResult (moneyGiven, chocolatePrice, wrapperValue);
+   }
+
+   /// <summary>
+   /// Gets input from user
+   /// </summary>
+   /// <param name="moneyGiven"></param>
+   /// <param name="chocolatePrice"></param>
+   /// <param name="wrapperValue"></param>
+   /// <returns></returns>
+   public static (int, int, int) GetInput (ref int moneyGiven, ref int chocolatePrice, ref int wrapperValue) {
+GetMoney: Console.Write ("Enter money : ");
+      if (!(int.TryParse (Console.ReadLine (), out moneyGiven) && moneyGiven > 0)) {
+         Console.WriteLine ("Give valid money");
+         goto GetMoney;
       }
+GetPrice: Console.Write ("Enter chocolate price : ");
+      if (!(int.TryParse (Console.ReadLine (), out chocolatePrice) && chocolatePrice > 0)) {
+         Console.WriteLine ("Give valid chocolate price");
+         goto GetPrice;
+      }
+GetWrapper: Console.Write ("Enter wrapper value : ");
+      if (!(int.TryParse (Console.ReadLine (), out wrapperValue) && wrapperValue > 1)) {
+         Console.WriteLine ("Give wrapper value greater than 1");
+         goto GetWrapper;
+      }
+      return (moneyGiven, chocolatePrice, wrapperValue);
+   }
+
+   /// <summary>
+   /// Prints results of number of chocolate, wrappers, remaining
+   /// </summary>
+   /// <param name="moneyGiven"></param>
+   /// <param name="chocolatePrice"></param>
+   /// <param name="wrapperValue"></param>
+   public static void GetResult (int moneyGiven, int chocolatePrice, int wrapperValue) {
+      var (extraWrapper, remaining) = (0, 0);
+      int chocolate = moneyGiven / chocolatePrice;
+      remaining += moneyGiven % chocolatePrice;
+      extraWrapper += chocolate;
+      while (extraWrapper >= wrapperValue) {
+         int extraChocolate = extraWrapper / wrapperValue;
+         extraWrapper %= wrapperValue;
+         chocolate += extraChocolate;
+         extraWrapper += extraChocolate;
+      }
+      Console.WriteLine ($"Total choclate : {chocolate}");
+      Console.WriteLine ($"Remaining wrapperValue : {extraWrapper}");
+      Console.WriteLine ($"Remaining moneyGiven : {remaining}");
    }
 }
