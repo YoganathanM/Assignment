da0f7af5225d8c678c06d05a4a186129ad262c76
yoganathan <yoganathan.m@metamation.com>
2024-01-06 15:41:33 +0530

    Changes applied.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,8 @@
 ﻿namespace Assignment {
    internal class Program {
       static void Main (string[] args) {
-         while(true) {
+         Console.WriteLine ("\x1B[4m" + "VOTING CONTEST" + "\x1B[0m");
+         while (true) {
             Console.Write ("Enter total votes : ");
             string input = Console.ReadLine ()!.ToLower ().Replace (" ", "");
             if (input.Length > 0) {
@@ -10,15 +11,33 @@
             } else Console.WriteLine ("Input can't be empty");
          }
       }
-
+      /// <summary>String provided by the user will be splitted into characters and which will be considered as vote</summary>
+      /// <param name="input">Maximum count will be the winning contestant</param>
       public static void VotingContext (string input) {
-         Dictionary<char, int> vote = new ();
-         for (int i = 0; i < input.Length; i++) {
-            if (vote.ContainsKey (input[i])) vote[input[i]]++;
-            else vote[input[i]] = 1;
+         List<char> contestant = new ();
+         List<int> totalVote = new ();
+         while (input.Length > 0) {
+            int inputLength = input.Length;
+            int count = 0, i = 0;
+            for (int j = i; j < inputLength; j++) {
+               if (input[i] == input[j]) count++;
+            }
+            contestant.Add (input[i]);
+            totalVote.Add (count);
+            string refactoredInput = "";
+            foreach (var a in input) {
+               if (a != input[i]) refactoredInput += a;
+            }
+            input = refactoredInput;
          }
-         var highestVote = vote.OrderByDescending (e => e.Value).First ();
-         Console.WriteLine ($"Winner is {highestVote.Key}");
+         int maximum = totalVote.Max ();
+         for (int l = 0; l < totalVote.Count; l++) {
+            if (totalVote[l] == maximum) {
+               Console.WriteLine ($"Winner is {contestant[l]}");
+               break;
+            }
+            break;
+         }
       }
    }
 }
