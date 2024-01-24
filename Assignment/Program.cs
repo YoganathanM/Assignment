d3d3fd5276d6f8f7c7fdaac54da2d4e0f50fe2d5
yoganathan <yoganathan.m@metamation.com>
2024-01-24 14:59:40 +0530

    Code cleanup

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -2,25 +2,21 @@
    internal class Program {
       static void Main (string[] args) {
          var words = File.ReadAllLines ("C:/etc/input.txt");
-         int countEntry = 0;
-         int count = 0;
          for (int i = 0; i < words.Length; i++) {
-            //Console.WriteLine (words[i]);
             //Splitting words based on :
+            int countEntry = 0;
+            int count = 0;
             if (words[i].Contains (":")) {
                string[] gameId = words[i].Split (':');
                for (int a = 0; a < gameId.Length; a++) {
-                  //Console.WriteLine (gameId[a]);
                   //Splitting words based on ;
                   if (gameId[a].Contains (";")) {
                      string[] eachSet = gameId[a].Split (";");
                      for (int b = 0; b < eachSet.Length; b++) {
-                        //Console.WriteLine (eachSet[b]);
                         //Splitting words based on , 
-                        if (eachSet[a].Contains (",")) {
-                           string[] eachBall = eachSet[a].Split (",");
+                        if (eachSet[b].Contains (",")) {
+                           string[] eachBall = eachSet[b].Split (",");
                            for (int c = 0; c < eachBall.Length; c++) {
-                              //Console.WriteLine (eachBall[c]);
                               //Splitting words based on empty space
                               if (eachBall[c].Contains (" ")) {
                                  string[] space = eachBall[c].Split (" ", StringSplitOptions.RemoveEmptyEntries);
